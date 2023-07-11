8faddb4cf979110dda2ca5bf97d6f558535cc92f
shanu sharma <shanu04012003@gmail.com>
2023-07-11 23:11:33 +0530

    link updated

--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -18,7 +18,7 @@ const Form = ({
     const handleSubmit = async(e) => {
         console.log('data :>> ', data);
         e.preventDefault()
-        const res = await fetch(`http://localhost:8000/${isSignInPage ? 'login' : 'register'}`, {
+        const res = await fetch(`https://chat-app-tau-three.vercel.app/${isSignInPage ? 'login' : 'register'}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
